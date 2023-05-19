0ceca1a274b4da869041b28e874b9e67c40eccb3
OTKRyu <appn12@gmail.com>
2023-05-19 17:29:42 +0900

    feat: add express setting and owner create api

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import morgan from "morgan"
+
+import ownerRouter from "./source/external/web/express/ownerRouter"
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+app.use("/", ownerRouter)
+
+app.listen(PORT)
---
+++ source/External/web/express/ownerRouter.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response} from "express"
+
+import OwnerCreateController from '../../../controller/ownerCreateController'
+import OwnerPermanenceImpl from 'source/external/permanence/mysql/implements/ownerPermanenceImpl';
+import AccountPermanenceImpl from 'source/external/permanence/mysql/implements/accountPermanenceImpl';
+import AccountImpl from 'source/usecase/accountImpl';
+
+const router = express.Router()
+
+router.post("", async (req: Request, res: Response) => {
+    try {
+        const balance: number = req.body.balance
+        const userName: string = req.body.userName
+    
+        const accountPermanence = new AccountPermanenceImpl()
+        const ownerPermanence = new OwnerPermanenceImpl(accountPermanence)
+        const ownerCreateController = new OwnerCreateController(ownerPermanence)
+    
+        const accountId = await accountPermanence.getNewAccountId()
+        const account = new AccountImpl(accountId, balance)
+    
+        const owner = ownerCreateController.createNewOwner(userName,account)
+    
+        res.status(201).json(owner)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+export default router
